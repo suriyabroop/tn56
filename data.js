75cc0a86741aef9b87a3ac2a5c16f0b6baf74e1e
suriyabroop <98744069+suriyabroop@users.noreply.github.com>
2022-03-11 18:54:44 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://tnvillanyt:suriyaskp12@cluster0.tzm8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+    
+    bot_token:'5250714235:AAGwRXMs5Y5C2ef6uRwS5_0LrHf1yHzE9CQ',//bot token
+    
+    bot_name:'LTC_V1KING_BOT',//bot username without @
+    
+    bot_admin:1996486622,// admin telegram id
+    
+    channelsList : ['@Airdropsstormchat','@bscprojects1','@RampageAirdrop','@MetaUSDOfficial','@Legend_Bsc','@payout_litecoin','@Legit_BotsOnly','@Live_AirdropHub'],// channels list
+    
+    reffer_bonus:0.00002,// refferal bonus amount
+    
+    min_wd:0.00004,//min withdrawal amount
+    
+    daily_bonus:0.000004,// daily bonus amount
+    
+    currency:'LTC', //bot cuurency
+    
+    cb_api_key:'ATICjx9N102levW0',//coinbase api key
+    
+    cb_api_secret:'m287xC7rEp7jToBhvU37aWcFzCHNZZga',// coinbase secret key
+    
+    cb_account_id:'bf5526cb-0163-5b2f-8f24-9238e5210c45',// counbase account id for the bot currency
+    
+    payment_channel:'@payout_litecoin'// payment channel username
+    
+    }
